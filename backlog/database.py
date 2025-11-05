ca92d97564461e833715efc8f3aaf545ec6fd49a
Vinícius Persike <persikejec@gmail.com>
2025-11-05 10:55:37 -0300

    Project setup and base database connection

---
+++ backlog/database.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+load_dotenv()
+
+#create a connection to the database.
+con = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
+#create a cursor in order to make queries to the database
+cursor = con.cursor()
+
+#Creates the table if it doesn't exist
+def create_table():
+    cursor.execute(
+    """CREATE TABLE IF NOT EXISTS games(
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        release_year INTEGER,
+        time_played INTEGER,
+        rating FLOAT
+        )"""
+    )
+    con.commit()
+
+def insert_game(title, year, time, rating):
+    values = (title, year, time, rating)
+    cursor.execute("INSERT INTO games VALUES(%s, %s, %s, %s)", values)
+    con.commit()
+
